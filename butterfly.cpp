c9f99b0fe080784d5be94b6ee3e22bc117f48911
shujaathussain009 <92740599+shujaathussain009@users.noreply.github.com>
2021-10-18 21:02:40 +0530

    Butterfly structure
    This program prints butterfly like structure  for the number of rows you want to print

---
+++ butterfly.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter the number of rows you want to print the butterfly like structure\t\t";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"00";
+        }
+        int space=2*n-2*i;
+        for(int j=1;j<=space;j++)
+        {
+            cout<<"  ";
+        }
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"00";
+        }
+        cout<<endl;
+    }
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"00";
+        }
+        int space=2*n-2*i;
+        for(int j=1;j<=space;j++)
+        {
+            cout<<"  ";
+        }
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"00";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
